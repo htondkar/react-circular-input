47c05306771b56beebd3123dc1d240b34e121628
Hamid Tondkar <htondkar@gmail.com>
2018-07-30 12:54:19 +0430

    big improvements

--- src/CircularTracker.js
+++ src/CircularTracker.js
@@ -4,21 +4,50 @@ export default class CircularMouseTracker extends React.Component {
   wrapper = React.createRef()
   indicator = React.createRef()
   radToDegCoefficient = 180 / Math.PI
+  tracking = false
 
   state = {
     degree: 0,
   }
 
   componentDidMount() {
+    window.addEventListener('mousedown', this.handleMouseDown)
+    window.addEventListener('mouseup', this.handleMouseUp)
     window.addEventListener('mousemove', this.handleMouseMove)
+    window.addEventListener('click', this.handleClick)
+    window.addEventListener('resize', this.calculateCenterOfSelf)
+
+    // initial calc
     this.calculateCenterOfSelf()
   }
 
   componentWillUnmount() {
+    window.removeEventListener('click', this.handleClick)
+    window.removeEventListener('mousedown', this.handleMouseDown)
+    window.removeEventListener('mouseup', this.handleMouseUp)
     window.removeEventListener('mousemove', this.handleMouseMove)
+    window.removeEventListener('resize', this.calculateCenterOfSelf)
+  }
+
+  handleMouseDown = event => {
+    event.preventDefault()
+    this.tracking = true
+  }
+
+  handleMouseUp = event => {
+    event.preventDefault()
+    this.tracking = false
+  }
+
+  handleClick = event => {
+    this.tracking = true
+    this.handleMouseMove(event)
+    this.tracking = false
   }
 
   handleMouseMove = ({ clientX, clientY }) => {
+    if (!this.tracking) return
+
     // get the distance between mouse and the center of wrapped children
     const deltaX = clientX - this.centerOfSelf.x
     const deltaY = clientY - this.centerOfSelf.y
@@ -30,14 +59,11 @@ export default class CircularMouseTracker extends React.Component {
   }
 
   calculateDegree = (dX, dY) => {
-    const radian = Math.atan2(dY, dX)
-    const degree = radian * this.radToDegCoefficient
+    const radian = Math.atan2(dY, dX) // number between -PI/2 to +PI/2
+    const degree = radian * this.radToDegCoefficient // number between -180deg to +180deg
 
-    if (this.isPositive(degree)) {
-      return degree
-    }
-
-    return degree + 360
+    // convert to 360deg instead of -180 to +180
+    return this.isPositive(degree) ? degree : degree + 360
   }
 
   isPositive = num => Math.sign(num) === 1
